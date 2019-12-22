406afc73b39d2e67743f95a268c1b4d5e75aac06
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 21:51:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -35,7 +35,8 @@ class CashRegister
   end
 
 
-  def void_last_transaction()
+  def void_last_transaction(price, quantity=1)
+      @last_transaction_amount = price * quantity
       @total -= @last_transaction_amount
       self.total = self.total - self.last_transaction_amount
       end
