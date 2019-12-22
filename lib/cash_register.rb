b5306a8d3fa20586a02afa6f7f750d53de852e5c
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 21:19:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -39,7 +39,7 @@ class CashRegister
       cash_register.add_item("apple", 0.99)
       cash_register.add_item("tomato", 1.76)
       cash_register.void_last_transaction
-      @total
+      @total - last_transaction_amount
   end
   
 end
